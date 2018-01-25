83465f6173d46a2c63d45236f2e1f666ee0cede8
Enrico Donelli <enrico.donelli@gmail.com>
2018-01-25 18:39:51 +0100

    migliorie

--- qariamodel.go
+++ qariamodel.go
@@ -6,58 +6,58 @@ import "fmt"
 type Stazione struct {
 	StazioneID int    `json:"StazioneID"`
 	Nome       string `json:"Nome"`
-	Url        string `json:"Url"`
-	// Inquinanti string `json:"Inquinanti"`
+	URL        string `json:"Url"`
 }
 
+// Stazioni elenco stazioni
 type Stazioni []Stazione
 
+// StazioniResp oggetto usato in response JSON
 type StazioniResp struct {
 	Stazioni []Stazione
 }
 
+// Misura definizione struttura Misura
 type Misura struct {
-	Id         int
+	ID         int
 	DataMisura string
 	Inquinante string
 	StazioneID int
 	Valore     float64
 }
 
+// MisureResp elenco misure in risposta JSON
 type MisureResp struct {
 	Misure []Misura
 }
 
-func ElencoStazioni() []Stazione {
+func costruisciURL(comuneID int, stazioneID int) string {
+	baseURL := "http://www.arpalombardia.it/sites/QAria/_layouts/15/QAria/DettaglioStazione.aspx?zona=BS&comune=%v&IdStaz=%v&isPDV=True"
+	return fmt.Sprintf(baseURL, comuneID, stazioneID)
+}
 
-	baseUrl := "http://www.arpalombardia.it/sites/QAria/_layouts/15/QAria/DettaglioStazione.aspx"
+// ElencoStazioni elenco delle stazioni
+func ElencoStazioni() []Stazione {
 
 	rezzato := Stazione{
 		StazioneID: 661,
 		Nome:       "Rezzato",
-		Url:        fmt.Sprintf("%s?zona=BS&comune=918&IdStaz=%s&isPDV=True", baseUrl, "661")}
+		URL:        costruisciURL(918, 661)}
 
 	milano := Stazione{
 		StazioneID: 539,
 		Nome:       "Milano Liguria",
-		Url:        fmt.Sprintf("%s?zona=MI&comune=451&IdStaz=%s&isPDV=True", baseUrl, "539")}
+		URL:        costruisciURL(451, 539)}
 
 	bresciaTurati := Stazione{
 		StazioneID: 652,
 		Nome:       "Brescia, via Turati",
-		Url:        fmt.Sprintf("%s?zona=BS&comune=918&IdStaz=%s&isPDV=True", baseUrl, "652")}
+		URL:        costruisciURL(918, 652)}
 
-	/*
-		bresciaGambara := Stazione{
-			StazioneId: 656,
-			Nome:       "Gambara (BS)",
-			Inquinanti: "NO2, O3",
-			Url:        fmt.Sprintf("%s?zona=BS&comune=830&IdStaz=%s&isPDV=True", baseUrl, "656")}
-	*/
 	cremonaCadorna := Stazione{
 		StazioneID: 627,
 		Nome:       "Cremona, Cadorna",
-		Url:        fmt.Sprintf("%s?zona=BS&comune=1189&IdStaz=%s&isPDV=True", baseUrl, "627")}
+		URL:        costruisciURL(1189, 627)}
 
 	stazioni := []Stazione{bresciaTurati, milano, rezzato, cremonaCadorna}
 	return stazioni
